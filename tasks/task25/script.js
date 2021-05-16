2eb3859a60fe690fb1b1f81e4241c8420f16b61f
WapBly <luminarexus@gmail.com>
2021-05-16 20:24:48 +0300

    fix task25 script

--- tasks/task25/script.js
+++ tasks/task25/script.js
@@ -1,9 +1,9 @@
 if(brouser == 'Chrome') {
     alert("You've got the Chrome!");
 } else if(brouser == 'Edge'
-    || == 'Firefox'
-    || == 'Opera'
-    || == 'Safari') {
+    || browser == 'Firefox'
+    || browser == 'Opera'
+    || browser == 'Safari') {
         alert("Okay we support these browsers too");
 } else {
     alert('We hope that this page looks ok!');
